610c0f6c971e2dbec40d0eed746d9f711ac23126
Alfred01969 <40476087+Alfred01969@users.noreply.github.com>
2022-11-30 07:59:28 -0300

    Commit

--- ControleDeContatos/Data/BancoContext.cs
+++ ControleDeContatos/Data/BancoContext.cs
@@ -1,6 +1,22 @@
-﻿namespace ControleDeContatos.Data
+﻿
+using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
 {
-    public class BancoContext
+    public class BancoContext : DbContext
     {
+        //COnstrutor
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        //Definir os DbSet - Configurar as tabelas (entidades)
+        //Classe ContatoModel servirá de base
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+
+
     }
 }
